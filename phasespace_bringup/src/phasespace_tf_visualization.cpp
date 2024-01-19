530f8405ed660a656f73f105b7afa050a0ef2a35
Shriram Damodaran <99411053+shriram272@users.noreply.github.com>
2024-01-19 18:57:05 +0530

    Update phasespace_tf_visualization.cpp

--- phasespace_bringup/src/phasespace_tf_visualization.cpp
+++ phasespace_bringup/src/phasespace_tf_visualization.cpp
@@ -1,79 +1,87 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
-#include <ros/ros.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/static_transform_broadcaster.h>
-#include <geometry_msgs/TransformStamped.h>
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 
-#include <phasespace_msgs/Rigids.h>
-#include <phasespace_msgs/Cameras.h>
+#include "phasespace_msgs/phasespace_msgs/msg/cameras.hpp"
+#include "phasespace_msgs/phasespace_msgs/msg/rigids.hpp"
 
-// Rigid topic callback function, will be called when there is new messages on the topic.
-// It also publishes tf of rigid bodies
-void poseRigidCallback(const phasespace_msgs::Rigids &msg) {
-	// Transform broadcaster
-	static tf2_ros::TransformBroadcaster br;
-	
-	for(size_t i = 0; i < msg.rigids.size(); i++) {
-		geometry_msgs::TransformStamped transformStamped;
+using namespace std::placeholders;
 
-		transformStamped.header.stamp = ros::Time::now();
-	  	transformStamped.header.frame_id = "phasespace_base";
-	  	transformStamped.child_frame_id = "robot_" + std::to_string(msg.rigids[i].id);
-	  	
-	  	transformStamped.transform.translation.x = msg.rigids[i].x;
-	  	transformStamped.transform.translation.y = msg.rigids[i].y;
-	  	transformStamped.transform.translation.z = msg.rigids[i].z;
+class PhasespaceTfVisualizationNode : public rclcpp::Node {
+public:
+    PhasespaceTfVisualizationNode() : Node("phasespace_tf_visualization") {
+        RCLCPP_INFO(this->get_logger(), "TF Publishing started.");
 
-	  	transformStamped.transform.rotation.x = msg.rigids[i].qx;
-	  	transformStamped.transform.rotation.y = msg.rigids[i].qy;
-	  	transformStamped.transform.rotation.z = msg.rigids[i].qz;
-	  	transformStamped.transform.rotation.w = msg.rigids[i].qw;
-	 
-	 		// Publishing the tf
-	  	br.sendTransform(transformStamped);
-  	}
-}
+        rigid_subscriber = this->create_subscription<phasespace_msgs::msg::Rigids>(
+            "/phasespace/rigids", 10,
+            std::bind(&PhasespaceTfVisualizationNode::poseRigidCallback, this, _1));
 
-// Camera topic callback function, will be called when there is new messages on the topic.
-// It also publishes static tf of cameras
-void staticCameraTf(const phasespace_msgs::Cameras &msg) {
-	// Static Transform broadcaster
-	static tf2_ros::StaticTransformBroadcaster static_br;
-	
-	for(size_t i = 0; i < msg.cameras.size(); i++) {
-		geometry_msgs::TransformStamped static_transformStamped;
+        camera_subscriber = this->create_subscription<phasespace_msgs::msg::Cameras>(
+            "/phasespace/cameras", 10,
+            std::bind(&PhasespaceTfVisualizationNode::staticCameraTf, this, _1));
 
-		static_transformStamped.header.stamp = ros::Time::now();
-	  	static_transformStamped.header.frame_id = "phasespace_base";
-	  	static_transformStamped.child_frame_id = "camera_" + std::to_string(msg.cameras[i].id);
+        tf_broadcaster = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+        static_tf_broadcaster = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+    }
 
-	  	static_transformStamped.transform.translation.x = msg.cameras[i].x;
-	  	static_transformStamped.transform.translation.y = msg.cameras[i].y;
-	  	static_transformStamped.transform.translation.z = msg.cameras[i].z;
+private:
+    void poseRigidCallback(const phasespace_msgs::msg::Rigids::SharedPtr msg) {
+        for (const auto& rigid : msg->rigids) {
+            geometry_msgs::msg::TransformStamped transformStamped;
 
-	  	static_transformStamped.transform.rotation.x = msg.cameras[i].qx;
-	  	static_transformStamped.transform.rotation.y = msg.cameras[i].qy;
-	  	static_transformStamped.transform.rotation.z = msg.cameras[i].qz;
-	  	static_transformStamped.transform.rotation.w = msg.cameras[i].qw;
-	 
-	 		// Publishing the static tf
-	  	static_br.sendTransform(static_transformStamped);
-	}
-}
+            transformStamped.header.stamp = this->get_clock()->now();
+            transformStamped.header.frame_id = "phasespace_base";
+            transformStamped.child_frame_id = "robot_" + std::to_string(rigid.id);
 
-int main(int argc, char** argv) {
-	// Initializing the phasespace tf visualization node
-  ros::init(argc, argv, "phasespace_tf_visualization");
-  ros::NodeHandle nh;
+            transformStamped.transform.translation.x = rigid.x;
+            transformStamped.transform.translation.y = rigid.y;
+            transformStamped.transform.translation.z = rigid.z;
 
-  ROS_INFO_STREAM("TF Publishing started.");
-  
-  // Rigid and Camera Sunscriber handler
-  ros::Subscriber rigidSub = nh.subscribe("/phasespace/rigids", 1000, poseRigidCallback);
-  ros::Subscriber cameraSub = nh.subscribe("/phasespace/cameras", 1000, staticCameraTf);
-  
-  ros::spin();  
-  return 0;
+            transformStamped.transform.rotation.x = rigid.qx;
+            transformStamped.transform.rotation.y = rigid.qy;
+            transformStamped.transform.rotation.z = rigid.qz;
+            transformStamped.transform.rotation.w = rigid.qw;
+
+            tf_broadcaster->sendTransform(transformStamped);
+        }
+    }
+
+    void staticCameraTf(const phasespace_msgs::msg::Cameras::SharedPtr msg) {
+        for (const auto& camera : msg->cameras) {
+            geometry_msgs::msg::TransformStamped static_transformStamped;
+
+            static_transformStamped.header.stamp = this->get_clock()->now();
+            static_transformStamped.header.frame_id = "phasespace_base";
+            static_transformStamped.child_frame_id = "camera_" + std::to_string(camera.id);
+
+            static_transformStamped.transform.translation.x = camera.x;
+            static_transformStamped.transform.translation.y = camera.y;
+            static_transformStamped.transform.translation.z = camera.z;
+
+            static_transformStamped.transform.rotation.x = camera.qx;
+            static_transformStamped.transform.rotation.y = camera.qy;
+            static_transformStamped.transform.rotation.z = camera.qz;
+            static_transformStamped.transform.rotation.w = camera.qw;
+
+            static_tf_broadcaster->sendTransform(static_transformStamped);
+        }
+    }
+
+    rclcpp::Subscription<phasespace_msgs::msg::Rigids>::SharedPtr rigid_subscriber;
+    rclcpp::Subscription<phasespace_msgs::msg::Cameras>::SharedPtr camera_subscriber;
+    std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
+    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster;
+};
+
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<PhasespaceTfVisualizationNode>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
